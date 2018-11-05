cb1691e3b1a076feef18aaed418b3f130a54dc58
george <gokereka@teamapt.com>
2018-11-05 18:48:31 +0100

    Added models Authors and Books

--- src/main/java/com/george/dev/spring5webapp/model/Book.java
+++ src/main/java/com/george/dev/spring5webapp/model/Book.java
@@ -28,16 +28,20 @@ public class Book {
     }
 
     public Book(String title, String isbn, String publisher) {
+
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
+
     }
 
     public Book(String title, String isbn, String publisher, Set<Author> authors) {
+
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
         this.authors = authors;
+        
     }
 
     public Long getId() {
