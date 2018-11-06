c87bbea9c7a81319826c37ce31c704e8a623b934
george <gokereka@teamapt.com>
2018-11-06 05:31:43 +0100

    Added the PublisherRepository Class

--- src/main/java/com/george/dev/spring5webapp/repository/PublisherRepository.java
+++ src/main/java/com/george/dev/spring5webapp/repository/PublisherRepository.java
@@ -4,6 +4,10 @@ import com.george.dev.spring5webapp.model.Publisher;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Created by George 5/11/2018
+ */
+
 @Repository
 public interface PublisherRepository extends CrudRepository<Publisher, Long> {
 }
