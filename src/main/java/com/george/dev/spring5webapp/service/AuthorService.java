e7b6cb20820774785148de9af46c96613c3170f9
george <gokereka@teamapt.com>
2018-11-07 22:15:19 +0100

    Added a service class for authors

---
+++ src/main/java/com/george/dev/spring5webapp/service/AuthorService.java
@@ -0,0 +1,11 @@
+package com.george.dev.spring5webapp.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by George 6/11/2018
+ */
+
+@Service
+public class AuthorService {
+}
